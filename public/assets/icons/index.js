92b42672215e3cdf6c93bbe9b2828a1419ff3433
hazhirmohammadi <hazhir6690@gmail.com>
2023-12-24 17:30:36 +0330

    Style: completed Header and Hero

--- public/assets/icons/index.js
+++ public/assets/icons/index.js
@@ -1,9 +1,14 @@
 import Logo from "./Logo.png";
+import LogoSm from "./Logo-sm.png";
 import cartShop from "./bag-nav.svg";
 import searchIcon from "./search-nav.svg";
+import menuIcon from "./menu.svg"
 
 export {
    Logo,
+   LogoSm,
    searchIcon,
-   cartShop
+   cartShop,
+   menuIcon,
+
 }
--- style.js
+++ style.js
@@ -1,3 +1,4 @@
-export const Style={
-   paddingX:"px-[64px]"
+export const Style = {
+   paddingX: "lg:px-[64px] px-6",
+   maximumSize: "max-w-[1440px] mx-auto"
 }
